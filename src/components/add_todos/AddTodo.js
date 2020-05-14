29e5b2daf4a3571750891d78b6d363ed1d007aa1
pmusole2 <pmusole2@gmail.com>
2020-05-14 22:40:09 +0200

    small change made

--- src/components/add_todos/AddTodo.js
+++ src/components/add_todos/AddTodo.js
@@ -1,43 +1,43 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import { addTodo } from '../../actions/todoActions';
 import { connect } from 'react-redux';
-import {v4 as uuid} from 'uuid'
+import { v4 as uuid } from 'uuid';
 
+const AddTodo = ({ addTodo }) => {
+	const [todo, setTodo] = useState('');
 
-const AddTodo = ({addTodo}) => {
-    const [todo, setTodo] = useState('')
+	const onSubmit = e => {
+		e.preventDefault();
+		const newTodo = {
+			id: uuid(),
+			todo: todo,
+			isCompleted: false,
+		};
+		addTodo(newTodo);
+		setTodo('');
+	};
 
-    const onSubmit = (e) => {
-        e.preventDefault()
-        const newTodo = {
-            id: uuid(),
-            todo: todo,
-            isCompleted: false
-        }
-        addTodo(newTodo)
-        setTodo('')
-    }
+	return (
+		<form onSubmit={onSubmit}>
+			<div className='card mt-4 mb-4'>
+				<div className='card-body'>
+					<div className='form-group'>
+						<h3>Add Todo</h3>
+						<input
+							className='form-control'
+							type='text'
+							value={todo}
+							onChange={e => setTodo(e.target.value)}
+							name='todo'
+							placeholder='Add a new Todo'
+							required
+						/>
+					</div>
+					<button className='btn btn-primary btn-block'>Add Todo</button>
+				</div>
+			</div>
+		</form>
+	);
+};
 
-    return (
-        <form onSubmit={onSubmit}>
-            <div className="card mt-4 mb-4">
-                <div className="card-body">
-                    <div className='form-group'>
-                        <h3>Add Todo</h3>
-                        <input
-                            className='form-control'
-                            type='text'
-                            value={todo}
-                            onChange={e => setTodo(e.target.value)}
-                            name='todo'
-                            placeholder='Add a new Todo'
-                        />
-                    </div>
-                    <button className='btn btn-primary btn-block'>Add Todo</button>
-                </div>
-            </div>
-        </form>
-    );
-}
-
-export default connect(null, {addTodo})(AddTodo)
+export default connect(null, { addTodo })(AddTodo);
